460ed857a5c5dab8a3c40384a925c2c556681222
José Prieto <jap25o94@gmail.com>
2020-04-08 23:37:31 -0400

    Listo ejercicio 2

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,11 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+    h = sigmoid(X * theta);
+    thetareg = [0;theta(2:size(theta))];
+    J = -(y' * log(h) + (1 - y)' * log(1 - h)) / m + lambda * (thetareg' * thetareg) / (2 * m);
 
-
-
-
+    grad = (X' * (h - y) + lambda * thetareg) / m
 
 % =============================================================
 
